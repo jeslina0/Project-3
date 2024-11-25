4d59d763cc05ef62a58bfb3fd5df795fb9bb4d0d
jeslina0 <jeslina.s@biits.in>
2024-11-25 20:56:06 +0530

    Add files via upload

---
+++ ecommerce.js
@@ -0,0 +1,28 @@
+import React from "react";
+const Context = React.createContext({});
+import Context from "./Context";
+export default Context;
+const withContext = WrappedComponent => {
+    const WithHOC = props => {
+      return (
+        <Context.Consumer>
+          {context => <WrappedComponent {...props} context={context} />}
+        </Context.Consumer>
+      );
+    };
+    return WithHOC;
+  };
+  export default withContext;
+  import withContext from "../withContext";
+
+const Cart = props => {
+};
+export default function Login() {
+  return <>Login</>
+}
+export default function AddProduct() {
+    return <>AddProduct</>
+  }
+  export default function Cart() {
+    return <>Cart</>
+  }
